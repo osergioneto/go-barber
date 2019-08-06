95b9aa5b898b9ebbc513851e40208be3ecd110f2
Sérgio Neto <sergio.deusdedith@gmail.com>
2019-08-06 20:03:29 -0300

    Login.

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,27 @@
+const { User } = require("../models");
+
+class SessionController {
+  create(req, res) {
+    return res.render("auth/login");
+  }
+
+  async store(req, res) {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      console.log("Usuário não encontrado");
+      return res.redirect("/");
+    }
+
+    if (!(await user.checkPassword(password))) {
+      console.log("Senha incorreta");
+      return res.redirect("/");
+    }
+
+    res.redirect("/app/dashboard");
+  }
+}
+
+module.exports = new SessionController();
--- src/routes.js
+++ src/routes.js
@@ -5,10 +5,14 @@ const upload = require("multer")(multerConfig);
 const routes = express.Router();
 
 const UserController = require("./app/controllers/UserController");
+const SessionController = require("./app/controllers/SessionController");
 
-routes.get("/", (req, res) => res.render("auth/login"));
+routes.get("/", SessionController.create);
+routes.post("/login", SessionController.store);
 
 routes.get("/signup", UserController.create);
 routes.post("/signup", upload.single("avatar"), UserController.store);
+
+routes.get("/app/dashboard", (req, res) => res.render("dashboard"));
 
 module.exports = routes;
