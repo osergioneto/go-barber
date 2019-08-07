a82d5c3ef02b740c4f8323c9363d74bca05018b7
Sérgio Neto <sergio.deusdedith@gmail.com>
2019-08-06 23:46:36 -0300

    Mostrando prestadores de serviço

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,8 @@ const routes = express.Router();
 
 const UserController = require("./app/controllers/UserController");
 const SessionController = require("./app/controllers/SessionController");
+const DashboardController = require("./app/controllers/DashboardController");
+const FileController = require("./app/controllers/FileController");
 
 const authMiddleware = require("./app/middlewares/auth");
 const guestMiddleware = require("./app/middlewares/guest");
@@ -17,6 +19,8 @@ routes.use((req, res, next) => {
   return next();
 });
 
+routes.get("/files/:file", FileController.show);
+
 routes.get("/", guestMiddleware, SessionController.create);
 routes.post("/login", SessionController.store);
 
@@ -25,9 +29,7 @@ routes.post("/signup", upload.single("avatar"), UserController.store);
 
 routes.use("/app", authMiddleware);
 
-routes.get("/app/dashboard", (req, res) => {
-  res.render("dashboard");
-});
+routes.get("/app/dashboard", DashboardController.index);
 
 routes.get("/app/logout", SessionController.destroy);
 
